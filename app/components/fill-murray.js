95ce015c2534995485f39cf94e485ccb47d6d536
Jesse Buchanan <hello@jessebuchanan.ca>
2015-02-27 23:46:23 -0500

    Implement {{fill-murray}} component

--- app/components/fill-murray.js
+++ app/components/fill-murray.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // defaults
+  height: 100,
+  width: 100,
+
+  src: function() {
+    var base = "http://www.fillmurray.com/";
+    return base + this.get('width') + '/' + this.get('height');
+  }.computed('height', 'width');
 });
